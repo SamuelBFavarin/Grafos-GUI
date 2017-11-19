94f2e85f3e26cb359892d547511e02b3a739dc07
Vinícius Adriano Machado <viniciusadrianomachado@gmail.com>
2017-11-19 18:11:08 -0200

    Avanços no Método
    É preciso entender o ponto de parada do while. Não sei como fazer pra busca parar e deixar de ficar em loop infnito.

--- js/grafosLib.js
+++ js/grafosLib.js
@@ -1265,6 +1265,29 @@
         return menor;
     };
 
+    Grafo.prototype.existeArcoNoCaminho = function(caminho, vertice1, vertice2){
+
+        for(var i = 0; i < caminho.lengh; i++){
+            if(caminho[i][0] == vertice1 && caminho[i][1] == vertice2){
+                return true;
+            }
+        }
+
+        return false;    
+    };
+
+    Grafo.prototype.somaValorA = function(caminho, vertice1, vertice2, a){
+
+       for(var i = 0; i < caminho.lengh; i++){
+            if(caminho[i][0] == vertice1 && caminho[i][1] == vertice2){
+                caminho[i][2] = caminho[i][2] + a;
+            }
+        }
+
+        return caminho;
+
+    };
+
     Grafo.prototype.fordFukerson = function (){
         
         var solucao = 0; //Criar um inteiro S para solução iniciado com 0.
@@ -1272,27 +1295,35 @@
         var caminho = this.montaCaminhoControle(); //Monta o caminho com ligação + peso
         this.atribuirGrafoOriginal(); // transforma grafo no modelo de grafo original
         var menor;
-        
-        console.log(caminho);
 
+        //Enquanto nao ser falso, ou enquanto existir um caminho...
         while(caminho){
             
+            //Busca o menor arco e soma na solução
             menor = this.retornaMenorArco(caminho);
             solucao += menor;
-
-            for(var i = 0; i < caminho.length; i++){
+            
+            //Precisei fazer isso pois da loop infinito no for 
+            var tamanho = caminho.length;
+            
+            for(var i = 0; i < tamanho; i++){
+                
                 caminho[i][2] = caminho[i][2] - menor;
 
-                /*
-                  Se existir um arco (v,u)
-                    Somar o valor de A no arco (v,u)
-                  Senão
-                    Criar um arco (v,u) com o valor de A
-                  */
+                if(this.existeArcoNoCaminho(caminho, caminho[i][1], caminho[i][0])){
+                    caminho = this.somaValorA(caminho, caminho[i][1], caminho[i][0], menor);
+                }else{
+                    var arcoVU = new Array();
+                    arcoVU[0] = caminho[i][1];
+                    arcoVU[1] = caminho[i][0];
+                    arcoVU[2] = menor;
+                    caminho.push(arcoVU);
+                }
+
             }
 
             caminho = this.montaCaminhoControle(); //Refaz tudo, dfs + controle
-        }
+       }
 
         return solucao;
 
